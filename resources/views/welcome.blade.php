830b8873394fd57f219a5a90da59599962385b21
Josh-dotcom-prog <edyangujoshua@gmail.com>
2025-09-17 17:56:10 +0300

    created tags section with its components

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,22 +1,36 @@
 <x-layout>
+    <div class="space-y-10">
+        {{-- featured jobs section --}}
+        <section>
+            <x-section-heading>Featured Jobs</x-section-heading>
+            <div class="grid lg:grid-cols-3 gap-8 mt-6">
+                <x-job-card />
+                <x-job-card />
+                <x-job-card />
+            </div>
+        </section>
 
-    {{-- featured jobs section --}}
-    <section>
-        <x-section-heading>Featured Jobs</x-section-heading>
-        <div class="grid lg:grid-cols-3 gap-8 mt-6">
-            <x-job-card />
-            <x-job-card />
-            <x-job-card />
-        </div>
-    </section>
+        {{-- Tags section --}}
+        <section>
+            <x-section-heading>Tags</x-section-heading>
 
-    {{-- Tags section --}}
-    <section>
-        <x-section-heading>Tags</x-section-heading>
-    </section>
+            <div class="mt-6 space-x-1">
+                <x-tag>Tag</x-tag>
+                <x-tag>Tag</x-tag>
+                <x-tag>Tag</x-tag>
+                <x-tag>Tag</x-tag>
+                <x-tag>Tag</x-tag>
+                <x-tag>Tag</x-tag>
+                <x-tag>Tag</x-tag>
+                <x-tag>Tag</x-tag>
+                <x-tag>Tag</x-tag>
+                <x-tag>Tag</x-tag>
+            </div>
+        </section>
 
-    {{-- Recent jobs section --}}
-    <section>
-        <x-section-heading>Tags</x-section-heading>
-    </section>
+        {{-- Recent jobs section --}}
+        <section>
+            <x-section-heading>Recent Jobs</x-section-heading>
+        </section>
+    </div>
 </x-layout>
